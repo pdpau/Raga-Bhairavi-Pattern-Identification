d943faa34ceebe7c3856adc97d9259fdb104d821
pdpau <56670577+pdpau@users.noreply.github.com>
2024-04-29 09:06:13 +0200

    main pipeline

---
+++ code/main.py
@@ -0,0 +1,15 @@
+from data import load_data, normalize
+
+# Dataset creation
+
+# Load data
+data = load_data(path)
+
+# Pre-processing
+norm_data = normalize(data)
+
+# Feature extraction
+
+# Modelling
+
+# ...
