2950c4ebff68d782862bd775ffcdb22527d82b60
pdpau <56670577+pdpau@users.noreply.github.com>
2024-05-09 19:37:57 +0200

    features file returns a df

--- code/scripts/characterization/features.py
+++ code/scripts/characterization/features.py
@@ -4,23 +4,10 @@ import numpy as np
 import pandas as pd
 # import dtw library
 
-def extract_features(data: np.array) -> np.array:
+def extract_features(data: pd.DataFrame) -> pd.DataFrame:
     """
     Main function for extracting features from the data
     """
-    return np.array([1, 2, 3, 4, 5])
+    features_df = pd.DataFrame()
 
-
-
-
-def euclidean_distance(data1: np.array, data2: np.array) -> float: # TODO: Implement this function using an already existing library for Euclidean distance
-    """
-    Compute the Euclidean distance between two signals
-    """
-    return np.linalg.norm(data1 - data2) #¿?
-
-def dtw(data1: np.array, data2: np.array) -> float: # TODO: Implement this function using an already existing library for DTW
-    """
-    Compute the Dynamic Time Warping distance between two signals
-    """
-    return 0.0
+    return features_df
